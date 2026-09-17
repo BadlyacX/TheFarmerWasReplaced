0afb3422fe3a6932bc6447b694ad2f14bcc73167
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2026-09-17 23:11:51 +0800

    Update 自動種稻草、樹、蘿蔔.py

--- 自動種稻草、樹、蘿蔔.py
+++ 自動種稻草、樹、蘿蔔.py
@@ -1,19 +1,15 @@
 item_num_base = 1200
-
 # --------------- methods ---------------
 def is_even(n):
 	return n % 2 == 0
-	
 def is_tree_placeble():
 	if is_even(get_pos_y() + get_pos_x()):
 		return True
 	else:
 		return False
-		
 def pour_water(level):
 	if get_water() <= level:
 		use_item(Items.Water)
-		
 def plant_crop_and_move(entities, ground_type, direction):
 		pour_water(0.5)
 		harvest()
@@ -21,15 +17,13 @@ def plant_crop_and_move(entities, ground_type, direction):
 			till()
 		plant(entities)
 		move(direction)
-
 # --------------- main program ---------------
 while True:
 	if can_harvest():
 		if num_items(Items.Hay) <= item_num_base:
 			for i in range(get_world_size()):
 				plant_crop_and_move(Entities.Grass, Grounds.Grassland, North)	
-			move(East)
-			
+			move(East)	
 		elif num_items(Items.Wood) <= item_num_base:
 			for i in range(get_world_size()):
 				if is_tree_placeble():
