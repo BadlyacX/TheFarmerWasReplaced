67f5ff4a59d3212f244420d0b05f7b805a33ad76
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2026-09-19 01:08:15 +0800

    Create 自動種南瓜.py

---
+++ 自動種南瓜.py
@@ -0,0 +1,52 @@
+item_num_base = 15000
+# --------------- methods ---------------
+def is_even(n):
+	return n % 2 == 0
+def is_tree_placeble():
+	if is_even(get_pos_y() + get_pos_x()):
+		return True
+	else:
+		return False
+def pour_water(level):
+	if get_water() <= level:
+		use_item(Items.Water)
+def plant_crop(entities, ground_type):
+		pour_water(0.5)
+		harvest()
+		if get_ground_type() != ground_type:
+			till()
+		plant(entities)
+def plant_crop_and_move(entities, ground_type, direction):
+		plant_crop(entities, ground_type)
+		if get_entity_type() != Entities.Pumpkin:
+			move(direction)
+		else:
+			while True:
+				if get_entity_type() == Entities.Dead_Pumpkin:
+					plant(Entities.Pumpkin)
+				elif can_harvest() and get_entity_type() == Entities.Pumpkin:
+					move(direction)
+					break
+# --------------- main program ---------------
+while True:
+	if can_harvest() and num_items(Items.Hay) <= item_num_base:
+		for i in range(get_world_size()):
+			plant_crop_and_move(Entities.Grass, Grounds.Grassland, North)	
+		move(East)	
+	elif can_harvest() and num_items(Items.Wood) <= item_num_base:
+		for i in range(get_world_size()):
+			if is_tree_placeble():
+				plant_crop_and_move(Entities.Tree, Grounds.Grassland, North)
+			else:
+				plant_crop_and_move(Entities.Bush, Grounds.Grassland, North)
+		move(East)
+	elif can_harvest() and num_items(Items.Carrot) <= item_num_base:
+		for i in range(get_world_size()):
+			plant_crop_and_move(Entities.Carrot, Grounds.Soil, North)
+		move(East)
+	elif can_harvest() and num_items(Items.Pumpkin) != item_num_base: # I write this deliberately
+		for i in range(get_world_size()):
+			plant_crop_and_move(Entities.Pumpkin, Grounds.Soil, North)
+		move(East)
+	else:
+		plant_crop_and_move(Entities.Pumpkin, Grounds.Soil, North)
